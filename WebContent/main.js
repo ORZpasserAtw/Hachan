afc11997044d6b7309316287e466140c21b31fd7
H KJ <61111psp@gmail.com>
2020-10-02 21:10:29 +0800

    Travel.html

--- WebContent/main.js
+++ WebContent/main.js
@@ -15,258 +15,184 @@ $('.cardview.accountId').on('press', function (e) {
 		location.reload();
 	}
 });
-
 $('select.TravelPri').change(function () {
-    document.getElementById('a').selectedIndex = 0;
-    document.getElementById('b').selectedIndex = 0;
-    $('select.TravelSub').hide();
-    $('div.my-gallery').hide();
-    $('div.navbar a.upperR').hide();
-    $('select.TravelSub').eq(0).show();
-    $('div.my-gallery').eq(0).show();
-    $('div.navbar a.upperR').eq(0).show();
-    if (this.selectedIndex > 0) {
-        $('select.TravelSub').eq(0).hide();
-        $('select.TravelSub').eq(this.selectedIndex).show();
+        $('div.travelshow').hide();
+        $('div.travelshow').eq(0).show();
+        $('div.navbar a.upperR').hide();
+        $('div.navbar a.upperR').eq(0).show();
+
+    if (document.getElementById("TravelPri").selectedIndex === 1 && 
+        document.getElementById("TravelSub").selectedIndex === 1) {
+        $('div.travelshow').hide();
+        $('div.travelshow').eq(1).show();
+        $('div.navbar a.upperR').hide();
+        $('div.navbar a.upperR').eq(1).show();
+    }
+    if (document.getElementById("TravelPri").selectedIndex === 1 && 
+        document.getElementById("TravelSub").selectedIndex === 2) {
+        $('div.travelshow').hide();
+        $('div.travelshow').eq(3).show();
+        $('div.navbar a.upperR').hide();
+        $('div.navbar a.upperR').eq(3).show();
+    }
+    if (document.getElementById("TravelPri").selectedIndex === 2 && 
+        document.getElementById("TravelSub").selectedIndex === 1) {
+        $('div.travelshow').hide();
+        $('div.travelshow').eq(5).show();
+        $('div.navbar a.upperR').hide();
+        $('div.navbar a.upperR').eq(5).show();
+    }
+    if (document.getElementById("TravelPri").selectedIndex === 2 && 
+        document.getElementById("TravelSub").selectedIndex === 2) {
+        $('div.travelshow').hide();
+        $('div.travelshow').eq(7).show();
+        $('div.navbar a.upperR').hide();
+        $('div.navbar a.upperR').eq(7).show();
+    }
+    if (document.getElementById("TravelPri").selectedIndex === 3 && 
+        document.getElementById("TravelSub").selectedIndex === 1) {
+        $('div.travelshow').hide();
+        $('div.travelshow').eq(9).show();
+        $('div.navbar a.upperR').hide();
+        $('div.navbar a.upperR').eq(9).show();
+    }
+    if (document.getElementById("TravelPri").selectedIndex === 3 && 
+        document.getElementById("TravelSub").selectedIndex === 2) {
+        $('div.travelshow').hide();
+        $('div.travelshow').eq(11).show();
+        $('div.navbar a.upperR').hide();
+        $('div.navbar a.upperR').eq(11).show();
     }
 }).change();
 
-
-document.getElementById('a').addEventListener('change', function (e) {
-    if (e.target.value === "海生館") {
-        $('div.my-gallery').hide();
+$('select.TravelSub').change(function () {
+    if (document.getElementById("TravelPri").selectedIndex === 1 && 
+        document.getElementById("TravelSub").selectedIndex === 1) {
+        $('div.travelshow').hide();
+        $('div.travelshow').eq(1).show();
         $('div.navbar a.upperR').hide();
-        $('div.my-gallery').eq(1).show();
         $('div.navbar a.upperR').eq(1).show();
-    }else if (e.target.value === "動物園") {
-        $('div.my-gallery').hide();
+    }
+    if (document.getElementById("TravelPri").selectedIndex === 1 && 
+        document.getElementById("TravelSub").selectedIndex === 2) {
+        $('div.travelshow').hide();
+        $('div.travelshow').eq(3).show();
         $('div.navbar a.upperR').hide();
-        $('div.my-gallery').eq(2).show();
-        $('div.navbar a.upperR').eq(2).show();
-    }else if (e.target.value === "動物之家") {
-        $('div.my-gallery').hide();
-        $('div.navbar a.upperR').hide();
-        $('div.my-gallery').eq(3).show();
         $('div.navbar a.upperR').eq(3).show();
-    }else if (e.target.value === "其他") {
-        $('div.my-gallery').hide();
+    }
+    if (document.getElementById("TravelPri").selectedIndex === 2 && 
+        document.getElementById("TravelSub").selectedIndex === 1) {
+        $('div.travelshow').hide();
+        $('div.travelshow').eq(5).show();
         $('div.navbar a.upperR').hide();
-        $('div.my-gallery').eq(4).show();
+        $('div.navbar a.upperR').eq(5).show();
     }
-});
-
-document.getElementById('b').addEventListener('change', function (e) {
-    if (e.target.value === "阿里山") {
-        $('div.my-gallery').hide();
-        $('div.my-gallery').eq(5).show();
-    }else if (e.target.value === "陽明山") {
-        alert(e.target.value);
+    if (document.getElementById("TravelPri").selectedIndex === 2 && 
+        document.getElementById("TravelSub").selectedIndex === 2) {
+        $('div.travelshow').hide();
+        $('div.travelshow').eq(7).show();
+        $('div.navbar a.upperR').hide();
+        $('div.navbar a.upperR').eq(7).show();
     }
-});
-
-//PhotoSwipe
-var initPhotoSwipeFromDOM = function(gallerySelector) {
-
-    // parse slide data (url, title, size ...) from DOM elements 
-    // (children of gallerySelector)
-    var parseThumbnailElements = function(el) {
-        var thumbElements = el.childNodes,
-            numNodes = thumbElements.length,
-            items = [],
-            figureEl,
-            linkEl,
-            size,
-            item;
-
-        for(var i = 0; i < numNodes; i++) {
-
-            figureEl = thumbElements[i]; // <figure> element
-
-            // include only element nodes 
-            if(figureEl.nodeType !== 1) {
-                continue;
-            }
-
-            linkEl = figureEl.children[0]; // <a> element
-
-            size = linkEl.getAttribute('data-size').split('x');
-
-            // create slide object
-            item = {
-                src: linkEl.getAttribute('href'),
-                w: parseInt(size[0], 10),
-                h: parseInt(size[1], 10)
-            };
-
-
-
-            if(figureEl.children.length > 1) {
-                // <figcaption> content
-                item.title = figureEl.children[1].innerHTML; 
-            }
-
-            if(linkEl.children.length > 0) {
-                // <img> thumbnail element, retrieving thumbnail url
-                item.msrc = linkEl.children[0].getAttribute('src');
-            } 
-
-            item.el = figureEl; // save link to element for getThumbBoundsFn
-            items.push(item);
-        }
-
-        return items;
-    };
-
-    // find nearest parent element
-    var closest = function closest(el, fn) {
-        return el && ( fn(el) ? el : closest(el.parentNode, fn) );
-    };
-
-    // triggers when user clicks on thumbnail
-    var onThumbnailsClick = function(e) {
-        e = e || window.event;
-        e.preventDefault ? e.preventDefault() : e.returnValue = false;
-
-        var eTarget = e.target || e.srcElement;
-
-        // find root element of slide
-        var clickedListItem = closest(eTarget, function(el) {
-            return (el.tagName && el.tagName.toUpperCase() === 'FIGURE');
-        });
-
-        if(!clickedListItem) {
-            return;
-        }
-
-        // find index of clicked item by looping through all child nodes
-        // alternatively, you may define index via data- attribute
-        var clickedGallery = clickedListItem.parentNode,
-            childNodes = clickedListItem.parentNode.childNodes,
-            numChildNodes = childNodes.length,
-            nodeIndex = 0,
-            index;
-
-        for (var i = 0; i < numChildNodes; i++) {
-            if(childNodes[i].nodeType !== 1) { 
-                continue; 
-            }
-
-            if(childNodes[i] === clickedListItem) {
-                index = nodeIndex;
-                break;
-            }
-            nodeIndex++;
-        }
-
-
-
-        if(index >= 0) {
-            // open PhotoSwipe if valid index found
-            openPhotoSwipe( index, clickedGallery );
-        }
-        return false;
-    };
-
-    // parse picture index and gallery index from URL (#&pid=1&gid=2)
-    var photoswipeParseHash = function() {
-        var hash = window.location.hash.substring(1),
-        params = {};
-
-        if(hash.length < 5) {
-            return params;
-        }
-
-        var vars = hash.split('&');
-        for (var i = 0; i < vars.length; i++) {
-            if(!vars[i]) {
-                continue;
-            }
-            var pair = vars[i].split('=');  
-            if(pair.length < 2) {
-                continue;
-            }           
-            params[pair[0]] = pair[1];
-        }
-
-        if(params.gid) {
-            params.gid = parseInt(params.gid, 10);
-        }
-
-        return params;
-    };
-
-    var openPhotoSwipe = function(index, galleryElement, disableAnimation, fromURL) {
-        var pswpElement = document.querySelectorAll('.pswp')[0],
-            gallery,
-            options,
-            items;
-
-        items = parseThumbnailElements(galleryElement);
-
-        // define options (if needed)
-        options = {
-
-            // define gallery index (for URL)
-            galleryUID: galleryElement.getAttribute('data-pswp-uid'),
-
-            getThumbBoundsFn: function(index) {
-                // See Options -> getThumbBoundsFn section of documentation for more info
-                var thumbnail = items[index].el.getElementsByTagName('img')[0], // find thumbnail
-                    pageYScroll = window.pageYOffset || document.documentElement.scrollTop,
-                    rect = thumbnail.getBoundingClientRect(); 
-
-                return {x:rect.left, y:rect.top + pageYScroll, w:rect.width};
-            }
-
-        };
-
-        // PhotoSwipe opened from URL
-        if(fromURL) {
-            if(options.galleryPIDs) {
-                // parse real index when custom PIDs are used 
-                // http://photoswipe.com/documentation/faq.html#custom-pid-in-url
-                for(var j = 0; j < items.length; j++) {
-                    if(items[j].pid == index) {
-                        options.index = j;
-                        break;
-                    }
-                }
-            } else {
-                // in URL indexes start from 1
-                options.index = parseInt(index, 10) - 1;
-            }
-        } else {
-            options.index = parseInt(index, 10);
-        }
-
-        // exit if index not found
-        if( isNaN(options.index) ) {
-            return;
-        }
-
-        if(disableAnimation) {
-            options.showAnimationDuration = 0;
-        }
-
-        // Pass data to PhotoSwipe and initialize it
-        gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
-        gallery.init();
-    };
-
-    // loop through all gallery elements and bind events
-    var galleryElements = document.querySelectorAll( gallerySelector );
-
-    for(var i = 0, l = galleryElements.length; i < l; i++) {
-        galleryElements[i].setAttribute('data-pswp-uid', i+1);
-        galleryElements[i].onclick = onThumbnailsClick;
+    if (document.getElementById("TravelPri").selectedIndex === 3 && 
+        document.getElementById("TravelSub").selectedIndex === 1) {
+        $('div.travelshow').hide();
+        $('div.travelshow').eq(9).show();
+        $('div.navbar a.upperR').hide();
+        $('div.navbar a.upperR').eq(9).show();
     }
-
-    // Parse URL and open gallery if it contains #&pid=3&gid=1
-    var hashData = photoswipeParseHash();
-    if(hashData.pid && hashData.gid) {
-        openPhotoSwipe( hashData.pid ,  galleryElements[ hashData.gid - 1 ], true, true );
+    if (document.getElementById("TravelPri").selectedIndex === 3 && 
+        document.getElementById("TravelSub").selectedIndex === 2) {
+        $('div.travelshow').hide();
+        $('div.travelshow').eq(11).show();
+        $('div.navbar a.upperR').hide();
+        $('div.navbar a.upperR').eq(11).show();
     }
-};
+}).change();
 
-// execute above function
-initPhotoSwipeFromDOM('.my-gallery');
+document.getElementById('Hate').onclick = function() {
+    if (document.getElementById("TravelPri").selectedIndex === 1 && 
+        document.getElementById("TravelSub").selectedIndex === 1) {
+        if ($('div.travelshow').eq(2).is(":visible")){
+            $('div.travelshow').hide();
+            $('div.travelshow').eq(1).show();
+            $('div.navbar a.upperR').hide();
+            $('div.navbar a.upperR').eq(1).show();
+        }else{
+            $('div.travelshow').hide();
+            $('div.travelshow').eq(2).show();
+            $('div.navbar a.upperR').hide();
+            $('div.navbar a.upperR').eq(2).show();
+        }
+    }
+    if (document.getElementById("TravelPri").selectedIndex === 1 && 
+        document.getElementById("TravelSub").selectedIndex === 2) {
+        if ($('div.travelshow').eq(4).is(":visible")){
+            $('div.travelshow').hide();
+            $('div.travelshow').eq(3).show();
+            $('div.navbar a.upperR').hide();
+            $('div.navbar a.upperR').eq(3).show();
+        }else{
+            $('div.travelshow').hide();
+            $('div.travelshow').eq(4).show();
+            $('div.navbar a.upperR').hide();
+            $('div.navbar a.upperR').eq(4).show();
+        }
+    }
+    if (document.getElementById("TravelPri").selectedIndex === 2 && 
+        document.getElementById("TravelSub").selectedIndex === 1) {
+        if ($('div.travelshow').eq(6).is(":visible")){
+            $('div.travelshow').hide();
+            $('div.travelshow').eq(5).show();
+            $('div.navbar a.upperR').hide();
+            $('div.navbar a.upperR').eq(5).show();
+        }else{
+            $('div.travelshow').hide();
+            $('div.travelshow').eq(6).show();
+            $('div.navbar a.upperR').hide();
+            $('div.navbar a.upperR').eq(6).show();
+        }
+    }
+    if (document.getElementById("TravelPri").selectedIndex === 2 && 
+        document.getElementById("TravelSub").selectedIndex === 2) {
+        if ($('div.travelshow').eq(8).is(":visible")){
+            $('div.travelshow').hide();
+            $('div.travelshow').eq(7).show();
+            $('div.navbar a.upperR').hide();
+            $('div.navbar a.upperR').eq(7).show();
+        }else{
+            $('div.travelshow').hide();
+            $('div.travelshow').eq(8).show();
+            $('div.navbar a.upperR').hide();
+            $('div.navbar a.upperR').eq(8).show();
+        }
+    }
+    if (document.getElementById("TravelPri").selectedIndex === 3 && 
+        document.getElementById("TravelSub").selectedIndex === 1) {
+        if ($('div.travelshow').eq(10).is(":visible")){
+            $('div.travelshow').hide();
+            $('div.travelshow').eq(9).show();
+            $('div.navbar a.upperR').hide();
+            $('div.navbar a.upperR').eq(9).show();
+        }else{
+            $('div.travelshow').hide();
+            $('div.travelshow').eq(10).show();
+            $('div.navbar a.upperR').hide();
+            $('div.navbar a.upperR').eq(10).show();
+        }
+    }
+    if (document.getElementById("TravelPri").selectedIndex === 3 && 
+        document.getElementById("TravelSub").selectedIndex === 2) {
+        if ($('div.travelshow').eq(12).is(":visible")){
+            $('div.travelshow').hide();
+            $('div.travelshow').eq(11).show();
+            $('div.navbar a.upperR').hide();
+            $('div.navbar a.upperR').eq(11).show();
+        }else{
+            $('div.travelshow').hide();
+            $('div.travelshow').eq(12).show();
+            $('div.navbar a.upperR').hide();
+            $('div.navbar a.upperR').eq(12).show();
+        }
+    }
+}
